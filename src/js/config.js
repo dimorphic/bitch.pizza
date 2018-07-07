99e4061c694d2606554c0e31872172ad6d8c01cb
Sabin Tudor <sabin@gmx.us>
2018-07-07 23:06:29 +0300

    [app] Add simple config

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// settings
+export const PIZZA_SPIN_TIME = 1000;
+export const PIZZA_DEFAULT_TIME = 5000;
+export const PIZZA_REFRESH_RATE = 5000;
+
+export const DEFAULT_GIFS = {
+  spin: '//media.giphy.com/media/prA6olJdnIDKg/giphy.gif',
+  // getme: '//media.giphy.com/media/3og0IILScwtNUFqGbK/giphy.gif',
+  // spin: './assets/pizza-spinner.gif',
+  // getme: './assets/pizza-getme-bitch.gif',
+};
